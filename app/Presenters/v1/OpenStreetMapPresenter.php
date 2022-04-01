7e2f0b6eb9ea75c31b7d149ed5d7e9ddf6e4cfce
Sniperov <snaiperov60@gmail.com>
2022-04-01 10:20:46 +0600

    OSM fix

--- app/Presenters/v1/OpenStreetMapPresenter.php
+++ app/Presenters/v1/OpenStreetMapPresenter.php
@@ -9,14 +9,16 @@ class OpenStreetMapPresenter extends BasePresenter
     public function list()
     {
         $house_number = isset($this->model['address']['house_number']) ? $this->model['address']['house_number'] : '';
-        $city = isset($this->model['address']['city'])? $this->model['address']['city'] : $this->model['address']['town'];
+        $town = isset($this->model['address']['town']) ? $this->model['address']['town'] : '';
+        $city = isset($this->model['address']['city']) ? $this->model['address']['city'] . ', ' : $town;
+        $road = isset($this->model['address']['road']) ? $this->model['address']['road'] . ', ' : '';
         return [
             'lat' => $this->model['lat'],
             'lon' => $this->model['lon'],
             'full_path' => 
                 $this->model['address']['country'] . ', ' 
-                . $city . ', '
-                . $this->model['address']['road'] . ', '
+                . $city
+                . $road
                 . $house_number,
         ];
     }
